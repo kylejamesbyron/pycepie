0efb069501bc19ba4261b62b528e60bfa2e2d30b
kylejamesbyron <markandrewisom@gmail.com>
2023-04-06 11:17:34 -0400

    need to test

--- addRecipe.py
+++ addRecipe.py
@@ -1,1 +1,9 @@
+import sqlite3
+connection = sqlite3.connect("recipes.db")
+cursor = connection.cursor()
 
+# To insert table
+newTable = input("Recipe: ")
+
+#input headers as list
+cursor.execute("CREATE TABLE newTable (id INTEGER PRIMARY KEY AUTOINCREMENT, ingredient TEXT, amount INTEGER, genre TEXT, measure TEXT)") 
